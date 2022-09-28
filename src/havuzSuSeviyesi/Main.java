d670da75097b946435ef98607f793de6e5e7a4c1
ibmtalJava <114569460+ibmtalJava@users.noreply.github.com>
2022-09-28 11:38:56 +0300

    werrtert

--- src/havuzSuSeviyesi/Main.java
+++ src/havuzSuSeviyesi/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//seviye > 0 && seviye <10  ||   
+		//seviye >=10 && seviye <20 |||| 
+		//seviye >=10 && seviye <20 |||| 
+		//seviye >=10 && seviye <20 |||| 
+		//seviye >=10 && seviye <20 |||| 
 	}
 
 }
